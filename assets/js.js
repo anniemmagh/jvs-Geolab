dcdacf610468601a265e949f30d346273c402749
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-06-05 15:39:49 +0400

    first

---
+++ assets/js.js
@@ -0,0 +1,38 @@
+// var name="commschool";
+// var name2=name;
+// name="test";
+// console.log(name2);
+
+// var name="commschool";
+// var names=[
+//     'commschool1', 'commschool2','commschool3',null,123,
+// ];
+// console.log(names.length);
+// var user=[
+//     'ani',
+//     'test@gmail.com',
+//     20
+// ];
+
+// var user ={
+// name: "ani",
+// email: 'test@gmail.com',
+// age:20,
+// adresses:['tbilisi','aleksidze 10']
+// };
+// user.age=30;
+// console.log(user['age']);//// analogiurad gamoitans user.age rom dagvewera egec
+// console.log(user.adresses[1]);///masividan meore elementis amogeba
+var user ={
+    name: "ani",
+    email: 'test@gmail.com',
+    age:20,
+    friends:[{
+        name:'user 1'
+    },
+    {
+        name:'user 2'
+    }
+    ]
+    };
+    console.log(user.friends[1].name);
