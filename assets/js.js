dddb10705fd31005e070ad15f7255cc2f602c49b
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-07-17 19:02:16 +0400

    .

--- assets/js.js
+++ assets/js.js
@@ -1,75 +1,77 @@
-var name="commschool";
-var name2=name;
-name="test";
-console.log(name2);
+// var name="commschool";
+// var name2=name;
+// name="test";
+// console.log(name2);
 
-var name="commschool";
-var names=[
-    'commschool1', 'commschool2','commschool3',null,123,
-];
-console.log(names.length);
-var user=[
-    'ani',
-    'test@gmail.com',
-    20
-];
+// var name="commschool";
+// var names=[
+//     'commschool1', 'commschool2','commschool3',null,123,
+// ];
+// console.log(names.length);
+// var user=[
+//     'ani',
+//     'test@gmail.com',
+//     20
+// ];
 
-var user ={
-name: "ani",
-email: 'test@gmail.com',
-age:20,
-adresses:['tbilisi','aleksidze 10']
-};
-user.age=30;
-console.log(user['age']);//// analogiurad gamoitans user.age rom dagvewera egec
-console.log(user.adresses[1]);///masividan meore elementis amogeba
-var key='age';
-var user ={
-    name: "ani",
-    email: 'test@gmail.com',
-    age:20,
-    friends:[{
-        name:'user 1'
-        age:31
-    },
-    {
-        name:'user 2'
-        age:13
-    }
-    ]
-    };
-    // console.log(user.friends[1].name);///
-    console.log(user.friends[1]['age']);
-var key='age';
-var user ={
-    name: "ani",
-    email: 'test@gmail.com',
-    age:20
-};
-var user2=user;
-console.log(user2.age);
+// var user ={
+// name: "ani",
+// email: 'test@gmail.com',
+// age:20,
+// adresses:['tbilisi','aleksidze 10']
+// };
+// user.age=30;
+// console.log(user['age']);//// analogiurad gamoitans user.age rom dagvewera egec
+// console.log(user.adresses[1]);///masividan meore elementis amogeba
+// var key='age';
+// var user ={
+//     name: "ani",
+//     email: 'test@gmail.com',
+//     age:20,
+//     friends:[{
+//         name:'user 1'
+//         age:31
+//     },
+//     {
+//         name:'user 2'
+//         age:13
+//     }
+//     ]
+//     };
+//     // console.log(user.friends[1].name);///
+//     console.log(user.friends[1]['age']);
+// var key='age';
+// var user ={
+//     name: "ani",
+//     email: 'test@gmail.com',
+//     age:20
+// };
+// var user2=user;
+// console.log(user2.age);
 
-var age=10;
-var name='giorgi';
-if(age>10 || name=='giorgi'){
-console.log('srulwlovani')
-}
-else{
-console.log('arasrulwlovani')
-console.log(age>=18);
-|| ori swori xazi nishnavs "an-s" da &&- es "da-s"
-var age=18;
-var name="test";
+// var age=10;
+// var name='giorgi';
+// if(age>10 || name=='giorgi'){
+// console.log('srulwlovani')
+// }
+// else{
+// console.log('arasrulwlovani')
+// console.log(age>=18);
+// || ori swori xazi nishnavs "an-s" da &&- es "da-s"
+// var age=18;
+// var name="test";
 
-if (! age=='18'){
-    console.log('srulwlovani');
-}
-else {
-    console.log('arasrulwlovani');
-}
-console.log(!true);/// ! abrunebs sapirispiros
-var age=18;
-var name='test';
-console.log(
-    '3'+'4'
-)/// stringand - '' tu vwert, miayolebs ertmanets
+// if (! age=='18'){
+//     console.log('srulwlovani');
+// }
+// else {
+//     console.log('arasrulwlovani');
+// }
+// console.log(!true);/// ! abrunebs sapirispiros
+// var age=18;
+// var name='test';
+// console.log(
+//     '3'+'4'
+// )/// stringand - '' tu vwert, miayolebs ertmanets
+
+for(i=0; i < 10; i++) { if ((i % 7) == 0) { print("Money") } else if ((i % 5) == 0) { print("Dolly") } }
